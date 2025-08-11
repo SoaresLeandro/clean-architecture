a19f6952191009fa1f1327d55cea9df718b6a59d
Leandro <lss.91@live.com>
2025-08-11 16:13:32 -0400

    Criando entidades

---
+++ CleanArchitecture.Domain/Accounts/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Shared.Entities;
+
+namespace CleanArchitecture.Domain.Accounts.Entities
+{
+    public class Student : Entity
+    {
+        #region Constructors
+        public Student(Guid? id) : base(id)
+        {
+        }
+        #endregion
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ CleanArchitecture.Domain/Shared/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchitecture.Domain.Shared.Entities
+{
+    public abstract class Entity(Guid? id) : IEquatable<Guid>
+    {
+        #region Properties
+        public Guid Id { get; } = id ?? Guid.CreateVersion7();
+        #endregion
+
+        #region Equatable Implementation
+        public bool Equals(Guid id) => Id.Equals(id);
+
+        public override int GetHashCode() => Id.GetHashCode();
+        #endregion
+    }
+}
