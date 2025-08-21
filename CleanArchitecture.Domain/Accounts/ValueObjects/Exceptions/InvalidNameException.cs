ecf5e5733206fafb37a7697160dc493a952d7cc4
Leandro da Silva Soares <lsilva@DEV030>
2025-08-21 17:07:38 -0400

    Exceptions customizadas

---
+++ CleanArchitecture.Domain/Accounts/ValueObjects/Exceptions/InvalidNameException.cs
@@ -0,0 +1,6 @@
+﻿using CleanArchitecture.Domain.Shared.Exceptions;
+
+namespace CleanArchitecture.Domain.Accounts.ValueObjects.Exceptions
+{
+    public sealed class InvalidNameException(string message) : DomainException(message);
+}
